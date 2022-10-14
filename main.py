abcc26c2361a2aa8e1d85f059f4374f9ecc6c4f1
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2022-10-14 18:51:17 +0400

    for conflict 2

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('There is a development version')
     print_hi('another hi')
-    print_hi('something NEW')
+    print_hi('somethinggg NEW')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
